3667c4cee7174b667adc22d10eaa727c0528b246
rinku-kr <rk24736599@gmail.com>
2023-11-29 00:48:49 +0530

    work with textEditingController

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -10,10 +10,12 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
-  var _enteredTitle = '';
+  final _titleController = TextEditingController();
 
-  void _saveTitleInput(String inputValue) {
-    _enteredTitle = inputValue;
+  @override
+  void dispose() {
+    _titleController.dispose();
+    super.dispose();
   }
 
   @override
@@ -22,7 +24,7 @@ class _NewExpenseState extends State<NewExpense> {
       padding: const EdgeInsets.all(10),
       child: Column(children: [
         TextField(
-          onChanged: _saveTitleInput,
+          controller: _titleController,
           maxLength: 50,
           decoration: const InputDecoration(
             label: Text('title'),
@@ -31,9 +33,7 @@ class _NewExpenseState extends State<NewExpense> {
         Row(
           children: [
             ElevatedButton(
-                onPressed: () {
-                  print(_enteredTitle);
-                },
+                onPressed: (() => print(_titleController.text)),
                 child: const Text("save expense"))
           ],
         )
