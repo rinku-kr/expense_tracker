f4b9b991213e28431c7a5419836f5a5a29bb5318
rinku-kr <rk24736599@gmail.com>
2023-11-29 00:02:13 +0530

    handling user text input with the textfield widget

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/models/expense.dart';
 import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
+import 'package:expense_tracker/widgets/new_expense.dart';
 
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
@@ -38,7 +39,7 @@ class _ExpensesState extends State<Expenses> {
   void _openAddExpenseOverlay() {
     showModalBottomSheet(
       context: context,
-      builder: (ctx) => const Text('modal_sheet!'),
+      builder: (ctx) => const NewExpense(),
     );
   }
 
