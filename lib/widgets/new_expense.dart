c14ef8161dad58d99f7af8e4bed81de226caf0c6
rinku-kr <rk24736599@gmail.com>
2023-11-29 23:11:22 +0530

    Showing a datePicker

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -13,6 +13,17 @@ class _NewExpenseState extends State<NewExpense> {
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
 
+  void _presentDatePicker() {
+    final now = DateTime.now();
+    final firstDate = DateTime(now.year - 1, now.month, now.day);
+
+    showDatePicker(
+        context: context,
+        initialDate: now,
+        firstDate: firstDate,
+        lastDate: now);
+  }
+
   @override
   void dispose() {
     _titleController.dispose();
@@ -32,13 +43,33 @@ class _NewExpenseState extends State<NewExpense> {
             label: Text('title'),
           ),
         ),
-        TextField(
-          controller: _amountController,
-          keyboardType: TextInputType.phone,
-          decoration: const InputDecoration(
-            label: Text('amount'),
-            prefixText: '\₹',
-          ),
+        Row(
+          children: [
+            Expanded(
+              child: TextField(
+                controller: _amountController,
+                keyboardType: TextInputType.phone,
+                decoration: const InputDecoration(
+                  label: Text('amount'),
+                  prefixText: '\₹',
+                ),
+              ),
+            ),
+            const SizedBox(
+              width: 10,
+            ),
+            Expanded(
+                child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                IconButton(
+                    onPressed: _presentDatePicker,
+                    icon: const Icon(Icons.calendar_month)),
+                const Text('Select Date')
+              ],
+            ))
+          ],
         ),
         Row(
           children: [
