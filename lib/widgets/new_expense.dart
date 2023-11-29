eeab48e9ffdabb923ca6add5e346fbcd73de1462
rinku-kr <rk24736599@gmail.com>
2023-11-29 23:36:31 +0530

    Handling data from the future

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:expense_tracker/models/expense.dart';
 
 class NewExpense extends StatefulWidget {
   const NewExpense({super.key});
@@ -13,15 +14,20 @@ class _NewExpenseState extends State<NewExpense> {
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
 
-  void _presentDatePicker() {
+  DateTime? _selectedDate;
+  void _presentDatePicker() async {
     final now = DateTime.now();
     final firstDate = DateTime(now.year - 1, now.month, now.day);
 
-    showDatePicker(
+    final pickedDate = await showDatePicker(
         context: context,
         initialDate: now,
         firstDate: firstDate,
         lastDate: now);
+
+    setState(() {
+      _selectedDate = pickedDate;
+    });
   }
 
   @override
@@ -66,7 +72,9 @@ class _NewExpenseState extends State<NewExpense> {
                 IconButton(
                     onPressed: _presentDatePicker,
                     icon: const Icon(Icons.calendar_month)),
-                const Text('Select Date')
+                Text(_selectedDate == null
+                    ? 'No Date Selected'
+                    : formatter.format(_selectedDate!))
               ],
             ))
           ],
