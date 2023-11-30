aea0d1bbebba58f22736ae45e4c953c3b2e128da
rinku-kr <rk24736599@gmail.com>
2023-12-01 01:19:48 +0530

    Validating user input & Showing an error dialog

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -31,6 +31,32 @@ class _NewExpenseState extends State<NewExpense> {
     });
   }
 
+  void _submitExpenseData() {
+    final enteredAmount = double.tryParse(_amountController
+        .text); // tryParse('data') => null, tryParse('12.3') => 12.3
+    final amountIsInvalid = enteredAmount == null || enteredAmount <= 0;
+    if (_titleController.text.trim().isEmpty ||
+        amountIsInvalid ||
+        _selectedDate == null) {
+      showDialog(
+        context: context,
+        builder: (ctx) => AlertDialog(
+          title: const Text('Invalid Input'),
+          content: const Text(
+              'Please make sure a valid title, amount,date and category was entered.'),
+          actions: [
+            TextButton(
+                onPressed: () {
+                  Navigator.pop(ctx);
+                },
+                child: const Text('Okay'))
+          ],
+        ),
+      );
+      return;
+    }
+  }
+
   @override
   void dispose() {
     _titleController.dispose();
@@ -103,8 +129,7 @@ class _NewExpenseState extends State<NewExpense> {
             const Spacer(),
             ElevatedButton(
                 onPressed: (() {
-                  print(_titleController.text);
-                  print(_amountController.text);
+                  _submitExpenseData;
                 }),
                 child: const Text("Save Expense")),
             const SizedBox(
