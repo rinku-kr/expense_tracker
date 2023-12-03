58aafb4b7aecd4b086c5c0cb50ec4bdc5a1bcdf9
rinku-kr <rk24736599@gmail.com>
2023-12-03 12:30:44 +0530

    Setting with text themes!

--- lib/main.dart
+++ lib/main.dart
@@ -7,11 +7,24 @@ var kColorScheme =
 void main() {
   return runApp(MaterialApp(
     theme: ThemeData().copyWith(
-        useMaterial3: true,
-        colorScheme: kColorScheme,
-        appBarTheme: const AppBarTheme().copyWith(
-            backgroundColor: kColorScheme.onPrimaryContainer,
-            foregroundColor: kColorScheme.onPrimary)),
+      useMaterial3: true,
+      colorScheme: kColorScheme,
+      appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.onPrimary),
+      cardTheme: const CardTheme().copyWith(
+        color: kColorScheme.secondary,
+        margin: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
+      ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+              backgroundColor: kColorScheme.primaryContainer)),
+      textTheme: ThemeData().textTheme.copyWith(
+          titleLarge: TextStyle(
+              fontSize: 15,
+              fontWeight: FontWeight.normal,
+              color: kColorScheme.onSecondaryContainer)),
+    ),
     home: const Expenses(),
   ));
 }
